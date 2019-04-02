4e579b127dcbcec0d8ec02ec97f0ab2eaa2e7fd6
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2019-04-02 10:34:08 +0200

    Boletin26

---
+++ src/boletin26/Boletin26.java
@@ -0,0 +1,15 @@
+package boletin26;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class Boletin26 {
+
+    public static void main(String[] args) {
+        Ventana1 obxVen1=new Ventana1();
+        
+        obxVen1.setVisible(true);
+    }
+    
+}
